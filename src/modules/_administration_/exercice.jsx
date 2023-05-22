ce46746534db20f018b587b10296763e0ebb6a2f
Rochnel P <69855926+rochnel1@users.noreply.github.com>
2023-05-22 06:51:38 +0100

    new commit

--- src/modules/_administration_/exercice.jsx
+++ src/modules/_administration_/exercice.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { OExercice, OPeriode } from "./_init_";
 import {
   TFormulaire,
@@ -10,41 +10,66 @@ import {
   TValidationButton,
   TFormList,
 } from "../../utils/__";
+import { ENDPOINTS } from "../../utils/Variables";
+import { api } from "../../utils/api";
+import { jsonDateConvert } from "../../utils/utils";
 
 export const Exercices = ({ children }) => {
-  const [itemsExo, setItemsExo] = useState([
-    { c1: 2013, c2: "01/01/2013", c3: "31/12/2013", c4: "En cours", c5: "Oui" },
-    { c1: 2013, c2: "01/01/2013", c3: "31/12/2013", c4: "En cours", c5: "Non" },
-  ]);
+  const [items, setItems] = useState([]);
+  const [refresh, setRefresh] = useState(false);
   //
-  const [open, setOpen] = useState({ exercice: false });
+  const [open, setOpen] = useState({ exercice: false, idexercice: 0 });
 
   const add = (e) => {
-    setOpen({ ...open, exercice: true });
+    setOpen({ ...open, exercice: true, idexercice: 0 });
+  };
+
+  const modify = (id) => {
+    setOpen({ ...open, exercice: true, idexercice: id });
   };
 
   const print = (e) => {
     // setOpen({ ...open, groupe: true });
     console.log("Impression");
   };
-  const remove = (e) => {
-    // setOpen({ ...open, groupe: true });
-    console.log("Suppression");
-  };
 
   const quit = (e) => {
-    setOpen({ ...open, exercice: false });
+    setOpen({ ...open, exercice: false, groupe: false });
   };
+
+  const rafraichir = () => {
+    setRefresh(!refresh);
+  };
+
+  const loadItems = () => {
+    api(ENDPOINTS.exercices)
+      .fetch()
+      .then((res) => setItems(res.data))
+      .catch((err) => alert(err));
+  };
+
+  useEffect(() => {
+    // console.log("Code With Rochnel");
+    loadItems();
+  }, [refresh]);
 
   return (
     <>
       <TFormList
         title="Liste des exercices"
-        options={<TValidationButton add={add} remove={remove} print={print} />}
+        options={
+          <TValidationButton add={add} print={print} refresh={rafraichir} />
+        }
       >
         <TTable
-          items={itemsExo}
-          columns={["c1", "c2", "c3", "c4", "c5"]}
+          items={items}
+          columns={[
+            { name: "code" },
+            { name: "datedebut" },
+            { name: "datefin" },
+            { name: "statut" },
+            { name: "cloture" },
+          ]}
           columnsDisplay={[
             "Code",
             "Date de Début",
@@ -52,12 +77,18 @@ export const Exercices = ({ children }) => {
             "Statut",
             "Cloturé",
           ]}
-          // columnsWidth={["120px", "auto"]}
+          lineClick={(o) => {
+            modify(o.idexercice);
+          }}
         ></TTable>
       </TFormList>
       {open.exercice && (
         <TModal>
-          <EExercices addQuiHandler={quit} />
+          <EExercices
+            itemId={open.idexercice}
+            addQuiHandler={quit}
+            addRefreshHandler={rafraichir}
+          />
         </TModal>
       )}
       <Periodes />
@@ -65,73 +96,76 @@ export const Exercices = ({ children }) => {
   );
 };
 
-export const Periodes = ({ children }) => {
-  const [items, setItems] = useState([
-    { c1: 2013, c2: "Avril", c3: "01/01/2013", c4: "31/12/2013" },
-  ]);
-  const [open, setOpen] = useState({ periode: false });
+export const EExercices = ({
+  children,
+  addQuiHandler,
+  itemId = 0,
+  addRefreshHandler,
+}) => {
+  const [item, setItem] = useState(OExercice);
+  console.log(item.idexercice);
 
-  const add = (e) => {
-    setOpen({ ...open, periode: true });
-  };
-
-  const print = (e) => {
-    // setOpen({ ...open, groupe: true });
-    console.log("Impression");
-  };
-  const remove = (e) => {
-    // setOpen({ ...open, groupe: true });
-    console.log("Suppression");
-  };
-
-  const quit = (e) => {
-    setOpen({ ...open, periode: false });
-  };
-
-  return (
-    <>
-      <TFormList
-        title="Liste des périodes"
-        options={<TValidationButton add={add} remove={remove} print={print} />}
-      >
-        <TTable
-          items={items}
-          columns={["c1", "c2", "c3", "c4"]}
-          columnsDisplay={[
-            "Exercice",
-            "Code période",
-            "Date de Début",
-            "Date de Fin",
-          ]}
-          // columnsWidth={["120px", "auto"]}
-        ></TTable>
-      </TFormList>
-      {open.periode && (
-        <TModal>
-          <EPeriodes addQuiHandler={quit} />
-        </TModal>
-      )}
-    </>
-  );
-};
-
-export const EExercices = ({ children, addQuiHandler }) => {
-  const [itemExo, setItemExo] = useState(OExercice);
   const changeHandler = (e) => {
-    setItemExo({ ...itemExo, [e.target.name]: e.target.value });
+    setItem({ ...item, [e.target.name]: e.target.value });
   };
-  const save = (e) => {
-    console.log(itemExo);
+
+  const save = async (e) => {
+    // console.log(itemId);
+    console.log(item.idexercice);
+    if (item.idexercice === 0) {
+      //nouvel enregistrement
+      delete item.idexercice;
+      await api(ENDPOINTS.exercices).post(item);
+    } else {
+      //modification
+      //await api(ENDPOINTS.exercices).put(item.idexercice, item);
+    }
+    setItem({ ...OExercice });
+    if (addRefreshHandler) addRefreshHandler();
+    if (addQuiHandler) addQuiHandler();
   };
+
+  const remove = async (e) => {
+    if (item.idexercice === 0) return;
+    const res = await api(ENDPOINTS.exercices).delete(item.idexercice, item);
+    if (addRefreshHandler) addRefreshHandler(res);
+    if (addQuiHandler) addQuiHandler();
+  };
+
+  const loadItems = () => {
+    api(ENDPOINTS.exercices)
+      .fetch()
+      .then((res) => setItem(res.data))
+      .catch((err) => alert(err));
+  };
+
+  useEffect(() => {
+    // console.log("Code With Rochnel");
+    setItem({ ...OExercice });
+    if (itemId !== 0) {
+      api(ENDPOINTS.exercices)
+        .fetchById(itemId)
+        .then((res) => setItem(res.data))
+        .catch((err) => alert(err));
+    }
+    loadItems();
+  }, []);
+
   return (
     <TFormulaire
       title="Nouvel Exercice"
-      valPanel={<TValidationButton add={save} cancel={addQuiHandler} />}
+      valPanel={
+        <TValidationButton
+          add={save}
+          cancel={addQuiHandler}
+          remove={(e) => (item.idUtilisateur !== 0 ? remove() : undefined)}
+        />
+      }
     >
       <TInput
         label="Code"
         name="code"
-        value={itemExo.code}
+        value={item.code}
         maxlength={60}
         addChange={changeHandler}
       />
@@ -139,16 +173,16 @@ export const EExercices = ({ children, addQuiHandler }) => {
         <TInput
           type="date"
           label="Date de début"
-          name="date_debut"
-          value={itemExo.date_debut}
+          name="datedebut"
+          value={jsonDateConvert(item.datedebut)}
           maxlength={60}
           addChange={changeHandler}
         />
         <TInput
           type="date"
           label="Date de fin"
-          name="date_fin"
-          value={itemExo.date_fin}
+          name="datefin"
+          value={jsonDateConvert(item.datefin)}
           maxlength={60}
           addChange={changeHandler}
         />
@@ -158,7 +192,7 @@ export const EExercices = ({ children, addQuiHandler }) => {
         label="En cours"
         type="checkbox"
         name="statut"
-        value={(itemExo.statut = "En cours")}
+        value={(item.statut = "En cours")}
         maxlength={60}
         addChange={changeHandler}
       />
@@ -166,12 +200,12 @@ export const EExercices = ({ children, addQuiHandler }) => {
         label="Cloturé"
         name="cloture"
         items={[
-          { value: false, label: "Non" },
-          { value: true, label: "Oui" },
+          { value: "Non", label: "Non" },
+          { value: "oui", label: "Oui" },
         ]}
         columnId="value"
         columnDisplay="label"
-        value={itemExo.cloture}
+        value={item.cloture}
         addChange={changeHandler}
       />
       {children}
@@ -179,36 +213,168 @@ export const EExercices = ({ children, addQuiHandler }) => {
   );
 };
 
-export const EPeriodes = ({ children, addQuiHandler }) => {
+export const Periodes = ({ children }) => {
+  const [items, setItems] = useState([]);
+  const [open, setOpen] = useState({ periode: false, idperiode: 0 });
+  const [refresh, setRefresh] = useState(false);
+
+  const add = (e) => {
+    setOpen({ ...open, periode: true, idperiode: 0 });
+  };
+
+  const modify = (id) => {
+    setOpen({ ...open, periode: true, idperiode: id });
+  };
+
+  const quit = (e) => {
+    setOpen({ ...open, periode: false, groupe: false });
+  };
+
+  const rafraichir = () => {
+    setRefresh(!refresh);
+  };
+
+  const loadItems = () => {
+    api(ENDPOINTS.periodes)
+      .fetch()
+      .then((res) => setItems(res.data))
+      .catch((err) => alert(err));
+  };
+
+  useEffect(() => {
+    // console.log("Code With Rochnel");
+    loadItems();
+  }, [refresh]);
+
+  //
+  const setBabalScript = (bab) => {
+    return <>{bab}</>;
+  };
+  return (
+    <>
+      <TFormList
+        title="Liste des périodes"
+        options={<TValidationButton add={add} refresh={rafraichir} />}
+      >
+        <TTable
+          items={items}
+          columns={[
+            {
+              name: "",
+              render: (o) => setBabalScript(o.idexerciceNavigation?.code),
+            },
+            { name: "codeperiode" },
+            { name: "datedebut" },
+            { name: "datefin" },
+          ]}
+          columnsDisplay={[
+            "Exercice",
+            "Code période",
+            "Date de Début",
+            "Date de Fin",
+          ]}
+          lineClick={(o) => {
+            modify(o.idperiode);
+          }}
+        ></TTable>
+      </TFormList>
+      {open.periode && (
+        <TModal>
+          <EPeriodes
+            addQuiHandler={quit}
+            itemId={open.idperiode}
+            addRefreshHandler={rafraichir}
+          />
+        </TModal>
+      )}
+    </>
+  );
+};
+
+export const EPeriodes = ({
+  children,
+  addQuiHandler,
+  itemId = 0,
+  addRefreshHandler,
+}) => {
   const [item, setItem] = useState(OPeriode);
+
   const changeHandler = (e) => {
     setItem({ ...item, [e.target.name]: e.target.value });
   };
-  const save = (e) => {
-    console.log(item);
+
+  const save = async (e) => {
+    if (item.idperiode === 0) {
+      //nouvel enregistrement
+      delete item.idperiode;
+      await api(ENDPOINTS.periodes).post(item);
+    } else {
+      //modification
+      await api(ENDPOINTS.periodes).put(item.idperiode, item);
+    }
+    setItem({ ...OPeriode });
+    if (addRefreshHandler) addRefreshHandler();
+    if (addQuiHandler) addQuiHandler();
   };
+
+  const remove = async (e) => {
+    if (item.idperiode === 0) return;
+    const res = await api(ENDPOINTS.periodes).delete(item.idperiode, item);
+    if (addRefreshHandler) addRefreshHandler(res);
+    if (addQuiHandler) addQuiHandler();
+  };
+
+  const [exos, setExos] = useState([]);
+
+  const loadItems = () => {
+    api(ENDPOINTS.exercices)
+      .fetch()
+      .then((res) => setExos(res.data))
+      .catch((err) => alert(err));
+  };
+
+  useEffect(() => {
+    // console.log("Code With Rochnel");
+    setItem({ ...OPeriode });
+    // alert(itemId);
+    if (itemId !== 0) {
+      api(ENDPOINTS.periodes)
+        .fetchById(itemId)
+        .then((res) => {
+          console.log(res.data);
+          setItem(res.data);
+          // alert(JSON.stringify(res.data));
+        })
+        .catch((err) => alert(err));
+    }
+    loadItems();
+  }, []);
+
   return (
     <TFormulaire
       title="Nouvelle période"
-      valPanel={<TValidationButton add={save} cancel={addQuiHandler} />}
+      valPanel={
+        <TValidationButton
+          add={save}
+          cancel={addQuiHandler}
+          remove={(e) => (item.idperiode !== 0 ? remove() : undefined)}
+        />
+      }
     >
       <TLayout cols="1fr 1fr">
         <TSelect
           label="Exercice"
           name="exercice"
-          items={[
-            { value: false, label: "2022" },
-            { value: true, label: "2023" },
-          ]}
-          columnId="value"
-          columnDisplay="label"
-          value={item.exercice}
+          items={exos}
+          columnId="idexercice"
+          columnDisplay="code"
+          value={item.idexercice}
           addChange={changeHandler}
         />
         <TInput
           label="Code période"
-          name="code_periode"
-          value={item.code_periode}
+          name="codeperiode"
+          value={item.codeperiode}
           maxlength={60}
           addChange={changeHandler}
         />
@@ -218,16 +384,16 @@ export const EPeriodes = ({ children, addQuiHandler }) => {
         <TInput
           type="date"
           label="Date de début"
-          name="date_debut"
-          value={item.date_debut}
+          name="datedebut"
+          value={jsonDateConvert(item.datedebut)}
           maxlength={60}
           addChange={changeHandler}
         />
         <TInput
           type="date"
           label="Date de fin"
-          name="date_fin"
-          value={item.date_fin}
+          name="datefin"
+          value={jsonDateConvert(item.datefin)}
           maxlength={60}
           addChange={changeHandler}
         />
